f16fa5ad0e18e4c55183243c026ec81f1cf7e955
gayoung park <casper_52@naver.com>
2018-09-19 16:10:09 +0900

    1

--- Teacher/src/web/BoardController.java
+++ Teacher/src/web/BoardController.java
@@ -1,12 +1,41 @@
 package web;
 
-import org.apache.log4j.Logger;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import dao.QBoardDAO;
+import domain.PageDTO;
+import domain.PageMaker;
+import lombok.extern.log4j.Log4j;
+import web.util.Converter;
 
-public class BoardController {
+@WebServlet(urlPatterns = "/admin/board/*")
+@Log4j
+public class BoardController extends AbstractController {
 
-	QBoardDAO dao = new QBoardDAO();
+	private QBoardDAO dao = new QBoardDAO();
+	
+	public String listGET(HttpServletRequest req, HttpServletResponse resp) throws Exception{
+		System.out.println("listGET............");
+		
+		PageDTO dto = PageDTO.of().setPage(Converter.getInt(req.getParameter("page"),1))
+				.setSize(Converter.getInt(req.getParameter("size"),10));
+		
+		int total = 320;
+		PageMaker pageMaker = new PageMaker(total, dto);
+		
+		req.setAttribute("pageMaker", pageMaker);
+		req.setAttribute("list", dao.getList(dto));
+		
+		return "boardlist";
+	}
+
+	@Override
+	public String getBasic() {
+	
+		return "admin/board/";
+	}
 
 	
 }
